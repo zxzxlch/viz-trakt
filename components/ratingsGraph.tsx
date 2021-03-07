7c8ad75950c44ae2e698f433cc60ae0a44dd643b
Ben C <zxzxlch@gmail.com>
2021-03-07 14:41:38 +0800

    Update graph styles

--- components/ratingsGraph.tsx
+++ components/ratingsGraph.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef } from 'react';
-import { select, line, max, scaleLinear, curveNatural, axisBottom, area } from 'd3';
+import { select, selectAll } from 'd3-selection';
+import { max } from 'd3-array';
+import { scaleLinear } from 'd3-scale';
+import { line, area, curveCardinal } from 'd3-shape';
 import { IRatingDistribution } from '../lib/types';
 
 type Props = {
@@ -20,7 +23,9 @@ export default function RatingsGraph({ rating, votes, distribution }: Props) {
     const svg = select(svgRef.current);
     const data = distribution;
     const margin = { top: 10, right: 5, bottom: 5, left: 5 };
+    const curve = curveCardinal;
 
+    // scales
     const x = scaleLinear()
       .domain([0, 9])
       .range([margin.left, w - margin.right]);
@@ -29,14 +34,25 @@ export default function RatingsGraph({ rating, votes, distribution }: Props) {
       .domain([0, max(data)])
       .range([h - margin.bottom, margin.top]);
 
+    // y-axis
+    svg
+      .select('.y-axis')
+      .attr('stroke', '#D1D5DB')
+      .attr('stroke-width', 0.5)
+      .attr('stroke-dasharray', '4 3')
+      .selectAll('line')
+      .data(y.ticks(4))
+      .join('line')
+      .attr('y1', (d) => 0.5 + y(d))
+      .attr('y2', (d) => 0.5 + y(d))
+      .attr('x1', margin.left)
+      .attr('x2', w - margin.right);
+
+    // draw line
     const ratingsLine = line<number>()
       .x((_d, index) => x(index))
       .y((d) => y(d))
-      .curve(curveNatural);
-
-    const yAxis = axisBottom(x);
-
-    svg.select('.x-axis').attr('transform', `translate(0, ${h})`).call(yAxis);
+      .curve(curve);
 
     svg
       .selectAll('.line')
@@ -45,24 +61,16 @@ export default function RatingsGraph({ rating, votes, distribution }: Props) {
       .attr('d', (d) => ratingsLine(d))
       .attr('class', 'line')
       .attr('fill', 'none')
-      .attr('stroke-width', 1.5)
+      .attr('stroke-width', 2.5)
       .attr('stroke', 'url(#line-gradient)');
 
     const ratingsArea = area<number>()
       .x((_d, index) => x(index))
       .y0((d) => y(d))
       .y1(y(0))
-      .curve(curveNatural);
+      .curve(curve);
 
-    svg
-      .selectAll('.area')
-      .data([data])
-      .join('path')
-      .attr('d', (d) => ratingsArea(d))
-      .attr('class', 'area')
-      .attr('fill', 'url(#line-gradient)')
-      .attr('fill-opacity', 0.2);
-
+    // draw line points
     svg
       .selectAll('.point')
       .data(data, (_d, index) => index)
@@ -70,31 +78,49 @@ export default function RatingsGraph({ rating, votes, distribution }: Props) {
       .attr('class', 'point')
       .attr('cx', (_d, index) => x(index))
       .attr('cy', (d) => y(d))
-      .attr('r', 1.5)
+      .attr('r', 2)
       .attr('fill', 'url(#line-gradient');
 
+    // draw area
     svg
-      .select('#line-gradient')
-      .attr('gradientUnits', 'userSpaceOnUse')
-      .attr('x1', x(0))
-      .attr('y1', y(y.domain()[1]))
-      .attr('x2', x(0))
-      .attr('y2', y(0))
-      .selectAll('stop')
-      .data(
-        [
-          { offset: '0%', color: 'magenta' },
-          { offset: '100%', color: 'gray' },
-        ],
-        (d) => d['offset'],
-      )
-      .join('stop')
-      .attr('offset', (d) => d.offset)
-      .attr('stop-color', (d) => d.color);
+      .selectAll('.area')
+      .data([data])
+      .join('path')
+      .attr('d', (d) => ratingsArea(d))
+      .attr('class', 'area')
+      .attr('fill', 'url(#fill-gradient)')
+      .attr('fill-opacity', 0.2);
+
+    const createGradient = (className: string) => {
+      return svg
+        .select(`#${className}`)
+        .attr('gradientUnits', 'userSpaceOnUse')
+        .attr('x1', x(0))
+        .attr('y1', y(0))
+        .attr('x2', x(0))
+        .attr('y2', y(y.domain()[1]))
+        .selectAll('stop')
+        .data(
+          [
+            { offset: '0%', color: '#D1D5DB', opacity: '0' },
+            { offset: '20%', color: '#93C5FD', opacity: '0.7' },
+            { offset: '65%', color: '#A78BFA', opacity: '0.8' },
+            { offset: '98%', color: '#F472B6', opacity: '1' },
+            { offset: '100%', color: '#F472B6', opacity: '1' },
+          ],
+          (d) => d['offset'],
+        )
+        .join('stop')
+        .attr('offset', (d) => d.offset)
+        .attr('stop-color', (d) => d.color);
+    };
+
+    createGradient('line-gradient').attr('stop-opacity', 1);
+    createGradient('fill-gradient').attr('stop-opacity', (d) => d.opacity);
   }, [rating, distribution]);
 
   return (
-    <div className="w-full h-full border border-gray-200 rounded-lg">
+    <div className="w-full h-full">
       <svg
         className="w-full h-full"
         ref={svgRef}
@@ -104,6 +130,7 @@ export default function RatingsGraph({ rating, votes, distribution }: Props) {
         <g className="x-axis"></g>
         <g className="y-axis"></g>
         <linearGradient id="line-gradient"></linearGradient>
+        <linearGradient id="fill-gradient"></linearGradient>
       </svg>
     </div>
   );
